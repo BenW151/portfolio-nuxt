f06e6e7a4e7d351b2c784661b274b3d40fd4d519
Ben <benward151@gmail.com>
2024-08-06 18:30:02 +1000

    functioning slug page and config

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,29 +1,35 @@
 <template>
-  <div>
-    <Header
-      imageUrl="/images/index-background.webp"
-      imageAlt="Laptop with code on desk"
-      title="Blog Title"
-      subtitle="Blog Subtitle."
-      headerClass="header-large">
-    </Header>
+  <div class="blog">
+    <ContentDoc v-slot="{ doc }">
+      <article>
+        <!-- Pass the content fields as props to the Header component -->
+        <Header
+          :imageUrl="doc.imageUrl"
+          :imageAlt="doc.imageAlt"
+          :title="doc.title"
+          :subtitle="doc.subtitle"
+          headerClass="header-large" />
 
-    <div class="container">
-      <ContentDoc />
-    </div>
+        <!-- Render the rest of the content -->
+        <div class="container">
+          <h3>{{ doc.sectionTitle }}</h3>
+          <div v-html="doc.sectionText"></div>
+        </div>
+      </article>
+    </ContentDoc>
   </div>
 </template>
 
 <script>
 export default {
   async asyncData({ $content, params, error }) {
-    console.log('asyncData is running'); // Check if asyncData is executed
+    console.log("asyncData is running"); // Check if asyncData is executed
     let post;
     try {
       post = await $content("blog", params.slug).fetch();
-      console.log('Fetched post:', post); // Log the post object
+      console.log("Fetched post:", post); // Log the post object
     } catch (e) {
-      console.error('Error fetching post:', e);
+      console.error("Error fetching post:", e);
       error({ message: "Blog Post not found", statusCode: 404 });
     }
 
@@ -32,8 +38,13 @@ export default {
     };
   },
   mounted() {
-    console.log('Component has been mounted'); // Confirm the component is mounting
-  }
+    console.log("Component has been mounted"); // Confirm the component is mounting
+  },
 };
 </script>
 
+<style>
+.blog .paragraph.medium {
+  display: none;
+}
+</style>
