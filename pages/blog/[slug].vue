629a3017a4d143cb0c90bdea340fc4df25c1b6d6
Ben <benward151@gmail.com>
2024-08-09 08:48:21 +1000

    moved related posts to component

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -51,69 +51,18 @@
           </LayoutGridContainer>
         </section>
 
-        <!-- Posts with 'hiking' Tag -->
-        <section class="related-posts">
-          <LayoutGridContainer v-if="relatedPosts && relatedPosts.length">
-            <h3>Related Posts</h3>
-            <div
-              v-for="post in relatedPosts"
-              :key="post._path"
-              class="blog-item">
-              <NuxtLink :to="post._path" class="post-link">
-                <!-- Display the image -->
-                <img
-                  :src="post.headerImageUrl"
-                  :alt="post.headerImageAlt"
-                  class="post-image" />
-                <!-- Display the title -->
-                <NuxtLink :to="post._path" class="post-title">{{
-                  post.title
-                }}</NuxtLink>
-                <p>{{ post.description }}</p>
-              </NuxtLink>
-            </div>
-          </LayoutGridContainer>
-        </section>
+        <BlogRelatedPosts />
       </article>
     </ContentDoc>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from "nuxt/app";
-
 const pageIndexLabels = [
   { title: "What To Do", link: "what-to-do" },
   { title: "Where To Stay", link: "where-to-stay" },
   { title: "What To See", link: "what-to-see" },
 ];
-
-const route = useRoute();
-const slug = route.params.slug;
-
-const { data: currentPost } = await useAsyncData("currentPost", async () => {
-  const post = await queryContent("blog")
-    .where({ _path: `/blog/${slug}` })
-    .findOne();
-  return post;
-});
-
-const { data: relatedPosts } = await useAsyncData("relatedPosts", async () => {
-  if (!currentPost.value || !currentPost.value.tag) return [];
-
-  const content = await queryContent("blog").find();
-
-  // Filter posts by the current post's tag
-  const filteredContent = content.filter(
-    (post) =>
-      post.tag &&
-      post.tag === currentPost.value.tag &&
-      post._path !== currentPost.value._path
-  );
-
-  // Return the top 4 posts with the matched tag
-  return filteredContent.slice(0, 4);
-});
 </script>
 
 <script>
@@ -143,64 +92,16 @@ export default {
   grid-column: 5 / 13;
 }
 
-.related-posts h3 {
-  grid-column: 5 / 16;
-  grid-row: 1;
-}
-
-.post-title {
-  font-size: var(--font-size-M);
-  font-family: var(--font-family-primary);
-  margin-right: auto;
-}
-
-.post-link::after {
-  display: none;
-}
-
-.post-image {
-  width: 100%;
-  height: 20vw;
-  object-fit: cover;
-}
-
-.blog-item:nth-child(2) {
-  grid-column: 1 / 5;
-  grid-row: 2;
-}
-
-.blog-item:nth-child(3) {
-  grid-column: 5 / 9;
-  grid-row: 2;
-}
-
-.blog-item:nth-child(4) {
-  grid-column: 9 / 13;
-  grid-row: 2;
-}
-
-.blog-item:nth-child(5) {
-  grid-column: 13 / 17;
-  grid-row: 2;
-}
-
 @media (max-width: 767px) {
   .blog-text {
     grid-column: 1 / 7;
   }
 
-  .post-image {
-    height: 60vw;
-  }
+}
+</style>
 
-  .post-title {
-    margin-bottom: 0;
-  }
-
-  .blog-item:nth-child(n),
-  .related-posts h3 {
-    grid-column: 1 / 7;
-    grid-row: auto;
-  }
+<style>
+.blog .paragraph.medium {
+  display: none;
 }
 </style>
